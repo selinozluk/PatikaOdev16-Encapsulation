c65b8b43e2fd62c364c1a43a5a7e304944e0714b
selinozluk <selinozluk36@gmail.com>
2025-07-13 18:07:52 +0300

    Add project files.

---
+++ Encapsulation/Araba.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+
+    // Araba sınıfı oluştu
+    public class Araba
+    {
+        // Brand özelliği: Arabanın markası
+        public string Brand {  get; set; }
+
+        // Model özelliği: Arabanın modeli
+        public string Model { get; set; }
+
+
+        // Color özelliği: Arabanın rengi
+        public string Color { get; set; }
+
+        // Kapı sayısı için kapsülleme kullandığım alan
+        int _doorCount;
+        
+        // DoorCount özelliği: dışarıdan erişilen kapsüllenmiş property
+        public int DoorCount  {
+
+            get
+            {
+                // Kapı sayısı dışarıdan okunduğunda bu blok çalışır
+                return _doorCount;
+
+            }
+            set
+            {
+                // Eğer 2 veya 4 girilirse değer kabul edilir
+             if (value == 2 || value == 4)
+                {
+                    _doorCount = value;
+                }
+             else
+                {
+                    // 2 veya 4 dışında bir sayı girildiğinde kullanıcıyı uyarır ve değeri -1 olarak atar
+                    Console.WriteLine("Kapı sayısı 2 veya 4 dışında bir değer alamaz, hatalı giriş yaptınız. Kapı değeri -1 olarak güncellendi.");
+                    _doorCount = -1;
+                }
+            }
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,37 @@
+﻿using Encapsulation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Araba sınıfından car1 adında bir nesne oluşturuldu
+        Araba car1 = new Araba();
+
+        // Kullanıcıdan marka bilgisi alınır
+        Console.WriteLine("Arabanın Markasını Girin: ");
+        car1.Brand = Console.ReadLine();
+
+        // Kullanıcıdan model bilgisi alınır
+        Console.WriteLine("Arabanın Modelini Girin: ");
+        car1.Model = Console.ReadLine();
+
+        // Kullanıcıdan renk bilgisi alınır
+        Console.WriteLine("Arabanın Rengini Girin: ");
+        car1.Color = Console.ReadLine();
+
+        // Kullanıcıdan kapı sayı bilgisi alınır ve int'e dönüştürülüp kapsüllü property'e atanır
+        Console.WriteLine("Arabanın Kapı Sayısını Girin: ");
+        car1.DoorCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("----------------------------------------------");
+
+        Console.WriteLine("Girilen bilgilere göre kaydınız aşağıdaki şekilde oluşturulmuştur.");
+
+        // Kullanıcıdan bilgilerin girilmesini ister
+        Console.WriteLine($"Arabanın Markası: {car1.Brand}");
+        Console.WriteLine($"Arabanın Modeli: {car1.Model}");
+        Console.WriteLine($"Arabanın Rengi: {car1.Color}");
+        Console.WriteLine($"Arabanın Kapı Sayısı: {car1.DoorCount}");
+    }
+}
